a292a6e46295c0567c82fbfbae17ace24057eb4c
huancheng.zhang <huancheng.zhang@zhangmen.com>
2020-08-11 17:06:54 +0800

    2020年7月21日19:35:50 company apt

---
+++ hahaha/src/main/java/com/example/hahaha/hahahaJava.java
@@ -0,0 +1,12 @@
+package com.example.hahaha;
+
+import java.util.ArrayList;
+
+public class hahahaJava {
+    ArrayList<Integer> list = new ArrayList<>();
+    public int testFun(int x) {
+        list.add(new Integer(0));
+        x = Math.abs(x);
+        return x+1;
+    }
+}
